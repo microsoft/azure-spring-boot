431e894f6496485034eead785285a38d89442ba9
Zhijun Zhao <zhijzhao@microsoft.com>
2017-06-30 10:26:28 +0800

    update license header

--- azure-storage-spring-boot-autoconfigure/src/test/java/com/microsoft/azure/autoconfigure/azurestorage/StorageAutoConfigurationTest.java
+++ azure-storage-spring-boot-autoconfigure/src/test/java/com/microsoft/azure/autoconfigure/azurestorage/StorageAutoConfigurationTest.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
+ * Licensed under the Apache License. See LICENSE in the project root for
  * license information.
  */
 package com.microsoft.azure.autoconfigure.azurestorage;
