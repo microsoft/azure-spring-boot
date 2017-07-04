387c2735c756a721f914c1c2c96ea7882ac34fac
Zhijun Zhao <zhijzhao@microsoft.com>
2017-07-04 17:20:20 +0800

    use MIT license for open source

--- azure-storage-spring-boot-autoconfigure/src/test/java/com/microsoft/azure/autoconfigure/azurestorage/StoragePropertiesTest.java
+++ azure-storage-spring-boot-autoconfigure/src/test/java/com/microsoft/azure/autoconfigure/azurestorage/StoragePropertiesTest.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the Apache License. See LICENSE in the project root for
+ * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
 package com.microsoft.azure.autoconfigure.azurestorage;
