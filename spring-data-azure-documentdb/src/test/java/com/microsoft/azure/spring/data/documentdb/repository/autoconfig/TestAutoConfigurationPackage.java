7478274a93a806d41cc86363fae23b578189534f
yungez <yungez@microsoft.com>
2017-07-31 12:01:27 +0800

    add unit tests for spring-data-documentdb (#54)
    * add unit test for spring-data-documentdb
    
    * fix license header in test file

---
+++ spring-data-azure-documentdb/src/test/java/com/microsoft/azure/spring/data/documentdb/repository/autoconfig/TestAutoConfigurationPackage.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.data.documentdb.repository.autoconfig;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(TestAutoConfigurationPackageRegistrar.class)
+public @interface TestAutoConfigurationPackage {
+
+    Class<?> value();
+
+}
