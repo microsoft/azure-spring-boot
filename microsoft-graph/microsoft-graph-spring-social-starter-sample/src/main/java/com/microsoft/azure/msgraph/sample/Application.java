0e8f4926107932c0c39d3f5030797b6034c32f9f
ZhijunZhao <zhijzhao@microsoft.com>
2017-09-06 14:13:57 +0800

    add sample keyword to sample package name to avoid namespace conflict with starter packages

---
+++ microsoft-graph/microsoft-graph-spring-social-starter-sample/src/main/java/com/microsoft/azure/msgraph/sample/Application.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.msgraph.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
