381f08219affa401c2fe6f9671e52ec6bbb56f22
ZhijunZhao <zhijzhao@microsoft.com>
2017-09-20 13:33:03 +0800

    fix package name

--- azure-starters/azure-ad-integration-spring-boot-starter/src/test/java/com/microsoft/azure/spring/boot/autoconfigure/aad/UserPrincipalTest.java
+++ azure-starters/azure-ad-integration-spring-boot-starter/src/test/java/com/microsoft/azure/spring/boot/autoconfigure/aad/UserPrincipalTest.java
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
-package com.microsoft.azure.autoconfigure.aad;
+package com.microsoft.azure.spring.boot.autoconfigure.aad;
 
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Assert;
