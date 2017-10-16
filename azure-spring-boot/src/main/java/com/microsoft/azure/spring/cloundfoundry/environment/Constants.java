5db1b8acabbedbb698d5df07bfeeb4614ed7045b
ZhijunZhao <zhijzhao@microsoft.com>
2017-10-16 13:34:12 +0800

    Relocate CloudFoundry support

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloundfoundry/environment/Constants.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloundfoundry/environment/Constants.java
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
-package com.microsoft.azure.cloudfoundry.service;
+package com.microsoft.azure.spring.cloundfoundry.environment;
 
 public class Constants {
     // Property namespaces
