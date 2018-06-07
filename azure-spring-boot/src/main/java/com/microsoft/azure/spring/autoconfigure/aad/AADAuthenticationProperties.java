32055d1c2ca7a1aaf81eee886c62fb12c69f787d
weiping <wepa@microsoft.com>
2018-06-07 11:11:16 +0800

    make description more accurate

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAuthenticationProperties.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAuthenticationProperties.java
@@ -24,12 +24,12 @@ public class AADAuthenticationProperties {
     private String environment;
     /**
      * Registered application ID in Azure AD.
-     * @NotEmpty only for SPA implicit mode
+     * @NotEmpty only required when OAuth2 authentication is done in front end
      */
     private String clientId;
     /**
      * API Access Key of the registered application.
-     * @NotEmpty only for SPA implicit mode
+     * @NotEmpty only required when OAuth2 authentication is done in front end
      */
     private String clientSecret;
     /**
