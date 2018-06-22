afa1fbd34e8072f3036a985735b035eb9c6ab68c
weiping <wepa@microsoft.com>
2018-06-22 15:11:10 +0800

    remove unnecessary bean definition (#363)

--- azure-spring-boot-samples/azure-active-directory-spring-boot-backend-sample/src/main/java/sample/aad/security/AADOAuth2LoginSecurityConfig.java
+++ azure-spring-boot-samples/azure-active-directory-spring-boot-backend-sample/src/main/java/sample/aad/security/AADOAuth2LoginSecurityConfig.java
@@ -6,24 +6,17 @@
 package sample.aad.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AADOAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @Autowired
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
@@ -36,10 +29,5 @@ public class AADOAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login()
                 .userInfoEndpoint()
                 .oidcUserService(oidcUserService);
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(this.clientRegistrationRepository);
     }
 }
