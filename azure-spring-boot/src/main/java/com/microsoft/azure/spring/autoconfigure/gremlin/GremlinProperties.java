f8e31a9eb06a4570d585094bccbd3c65096c8197
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-06-26 19:06:04 +0800

    Add auto configuration for spring-data-gremlin. (#365)
    * Add auto configuration for spring-data-gremlin.
    
    * create Auto config when endpoint, port, username and password.
    
    * create Auto config when Factory, Template and Converter.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/gremlin/GremlinProperties.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.autoconfigure.gremlin;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("gremlin")
+public class GremlinProperties {
+
+    @NotEmpty
+    private String endpoint;
+
+    @NotEmpty
+    private String port;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    private boolean telemetryAllowed = true;
+}
