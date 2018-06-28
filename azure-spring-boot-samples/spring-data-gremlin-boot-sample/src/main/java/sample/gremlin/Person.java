f6dc6bca700fb91a5b97481ace1f202a2fd4772d
Pan Li <panli@microsoft.com>
2018-06-28 15:54:16 +0800

    Add stater and sample code for gremlin autoconfiguration.
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ azure-spring-boot-samples/spring-data-gremlin-boot-sample/src/main/java/sample/gremlin/Person.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package sample.gremlin;
+
+import com.microsoft.spring.data.gremlin.annotation.Vertex;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Vertex
+@Data
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private int level;
+}
