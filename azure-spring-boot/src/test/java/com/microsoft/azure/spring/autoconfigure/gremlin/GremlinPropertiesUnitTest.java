5c02fca9e3005727984797df4a39118bd1b9b599
Pan Li <panli@microsoft.com>
2018-06-29 10:55:08 +0800

    Refine the gremlin autoconfig with ApplicationContextRunner.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/GremlinPropertiesUnitTest.java
+++ azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/GremlinPropertiesUnitTest.java
@@ -6,35 +6,31 @@
 package com.microsoft.azure.spring.autoconfigure.gremlin;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static com.microsoft.azure.spring.autoconfigure.gremlin.PropertiesUtil.*;
 
 public class GremlinPropertiesUnitTest {
 
-    @Before
-    public void setup() {
-        PropertiesUtil.setPropertiesWithNoTelemetry();
-    }
-
-    @Before
-    public void cleanup() {
-        PropertiesUtil.cleanPropertiesWithNoTelemetry();
-    }
+    private ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(GremlinAutoConfiguration.class));
 
     @Test
     public void testAllProperties() {
-        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
-        context.register(GremlinAutoConfiguration.class);
-        context.refresh();
-
-        final GremlinProperties properties = context.getBean(GremlinProperties.class);
-
-        Assert.assertEquals(properties.getEndpoint(), PropertiesUtil.GREMLIN_ENDPOINT);
-        Assert.assertEquals(properties.getPort(), PropertiesUtil.GREMLIN_PORT);
-        Assert.assertEquals(properties.getUsername(), PropertiesUtil.GREMLIN_USERNAME);
-        Assert.assertEquals(properties.getPassword(), PropertiesUtil.GREMLIN_PASSWORD);
-        Assert.assertEquals(String.valueOf(properties.isTelemetryAllowed()), PropertiesUtil.GREMLIN_TELEMETRYALLOWED);
+        this.contextRunner
+                .withPropertyValues(GREMLIN_ENDPOINT_CONFIG)
+                .withPropertyValues(GREMLIN_PORT_CONFIG)
+                .withPropertyValues(GREMLIN_USERNAME_CONFIG)
+                .withPropertyValues(GREMLIN_PASSWORD_CONFIG)
+                .withPropertyValues(GREMLIN_TELEMETRY_CONFIG_NOT_ALLOWED)
+                .run(context -> {
+                    Assert.assertEquals(context.getBean(GremlinProperties.class).getEndpoint(), ENDPOINT);
+                    Assert.assertEquals(context.getBean(GremlinProperties.class).getPort(), PORT);
+                    Assert.assertEquals(context.getBean(GremlinProperties.class).getUsername(), USERNAME);
+                    Assert.assertEquals(context.getBean(GremlinProperties.class).getPassword(), PASSWORD);
+                    Assert.assertFalse(context.getBean(GremlinProperties.class).isTelemetryAllowed());
+                });
     }
 }
