d849695d0d6571873e3adcb1e709e10809cdb175
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-07-05 11:39:44 +0800

    Refine the gremlin autoconfig with ApplicationContextRunner, #375. (PR #376)
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

--- azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/GremlinAutoConfigurationUnitTest.java
+++ azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/GremlinAutoConfigurationUnitTest.java
@@ -9,47 +9,32 @@ import com.microsoft.spring.data.gremlin.common.GremlinFactory;
 import com.microsoft.spring.data.gremlin.conversion.MappingGremlinConverter;
 import com.microsoft.spring.data.gremlin.mapping.GremlinMappingContext;
 import com.microsoft.spring.data.gremlin.query.GremlinTemplate;
-import org.apache.tinkerpop.gremlin.driver.Client;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static com.microsoft.azure.spring.autoconfigure.gremlin.PropertiesUtil.*;
 
 public class GremlinAutoConfigurationUnitTest {
 
-    @Before
-    public void setup() {
-        PropertiesUtil.setProperties();
-    }
-
-    @Before
-    public void cleanup() {
-        PropertiesUtil.cleanProperties();
-    }
+    private ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(GremlinAutoConfiguration.class));
 
     @Test
     public void testAllBeanCreated() {
-        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
-        context.register(GremlinAutoConfiguration.class);
-        context.refresh();
-
-        final GremlinFactory factory = context.getBean(GremlinFactory.class);
-        Assert.assertNotNull(factory);
-
-        final Client client = factory.getGremlinClient();
-        Assert.assertNotNull(client);
-
-        final GremlinTemplate template = context.getBean(GremlinTemplate.class);
-        Assert.assertNotNull(template);
-
-        final GremlinMappingContext mappingContext = context.getBean(GremlinMappingContext.class);
-        Assert.assertNotNull(mappingContext);
-
-        final MappingGremlinConverter converter = context.getBean(MappingGremlinConverter.class);
-        Assert.assertNotNull(converter);
-
-        Assert.assertEquals(template.getMappingConverter(), converter);
-        Assert.assertEquals(converter.getMappingContext(), mappingContext);
+        this.contextRunner
+                .withPropertyValues(GREMLIN_ENDPOINT_CONFIG)
+                .withPropertyValues(GREMLIN_PORT_CONFIG)
+                .withPropertyValues(GREMLIN_USERNAME_CONFIG)
+                .withPropertyValues(GREMLIN_PASSWORD_CONFIG)
+                .withPropertyValues(GREMLIN_TELEMETRY_CONFIG_ALLOWED)
+                .run(context -> {
+                    Assert.assertNotNull(context.getBean(GremlinFactory.class));
+                    Assert.assertNotNull(context.getBean(GremlinFactory.class).getGremlinClient());
+                    Assert.assertNotNull(context.getBean(GremlinTemplate.class));
+                    Assert.assertNotNull(context.getBean(GremlinMappingContext.class));
+                    Assert.assertNotNull(context.getBean(MappingGremlinConverter.class));
+                });
     }
 }
--- azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/PropertiesUtil.java
+++ azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/gremlin/PropertiesUtil.java
@@ -6,42 +6,21 @@
 package com.microsoft.azure.spring.autoconfigure.gremlin;
 
 public class PropertiesUtil {
+    private static final String PROPERTY_ENDPOINT = "gremlin.endpoint";
+    private static final String PROPERTY_PORT = "gremlin.port";
+    private static final String PROPERTY_USERNAME = "gremlin.username";
+    private static final String PROPERTY_PASSWORD = "gremlin.password";
+    private static final String PROPERTY_TELEMETRY = "gremlin.telemetryAllowed";
 
-    public static final String PROPERTY_ENDPOINT = "gremlin.endpoint";
-    public static final String PROPERTY_PORT = "gremlin.port";
-    public static final String PROPERTY_USERNAME = "gremlin.username";
-    public static final String PROPERTY_PASSWORD = "gremlin.password";
-    public static final String PROPERTY_TELEMETRYALLOWED = "gremlin.telemetryAllowed";
+    public static final String ENDPOINT = "pli-gremlin-3-29.gremlin.cosmosdb.azure.com";
+    public static final String PORT = "443";
+    public static final String USERNAME = "/dbs/pli-database/colls/pli-collection";
+    public static final String PASSWORD = "xxxx";
 
-    public static final String GREMLIN_ENDPOINT = "pli-gremlin-3-29.gremlin.cosmosdb.azure.com";
-    public static final String GREMLIN_PORT = "443";
-    public static final String GREMLIN_USERNAME = "/dbs/pli-database/colls/pli-collection";
-    public static final String GREMLIN_PASSWORD = "xxxx";
-    public static final String GREMLIN_TELEMETRYALLOWED = "false";
-
-    public static void setProperties() {
-        System.setProperty(PROPERTY_ENDPOINT, GREMLIN_ENDPOINT);
-        System.setProperty(PROPERTY_PORT, GREMLIN_PORT);
-        System.setProperty(PROPERTY_USERNAME, GREMLIN_USERNAME);
-        System.setProperty(PROPERTY_PASSWORD, GREMLIN_PASSWORD);
-    }
-
-    public static void cleanProperties() {
-        System.clearProperty(PROPERTY_ENDPOINT);
-        System.clearProperty(PROPERTY_PORT);
-        System.clearProperty(PROPERTY_USERNAME);
-        System.clearProperty(PROPERTY_PASSWORD);
-    }
-
-    public static void setPropertiesWithNoTelemetry() {
-        setProperties();
-
-        System.setProperty(PROPERTY_TELEMETRYALLOWED, GREMLIN_TELEMETRYALLOWED);
-    }
-
-    public static void cleanPropertiesWithNoTelemetry() {
-        cleanProperties();
-
-        System.clearProperty(PROPERTY_TELEMETRYALLOWED);
-    }
+    public static final String GREMLIN_ENDPOINT_CONFIG = PROPERTY_ENDPOINT + "=" + ENDPOINT;
+    public static final String GREMLIN_PORT_CONFIG = PROPERTY_PORT + "=" + PORT;
+    public static final String GREMLIN_USERNAME_CONFIG = PROPERTY_USERNAME + "=" + USERNAME;
+    public static final String GREMLIN_PASSWORD_CONFIG = PROPERTY_PASSWORD + "=" + PASSWORD;
+    public static final String GREMLIN_TELEMETRY_CONFIG_NOT_ALLOWED = PROPERTY_TELEMETRY + "=" + "false";
+    public static final String GREMLIN_TELEMETRY_CONFIG_ALLOWED = PROPERTY_TELEMETRY + "=" + "true";
 }
