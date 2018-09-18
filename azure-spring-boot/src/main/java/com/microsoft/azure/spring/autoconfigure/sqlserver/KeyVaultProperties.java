fb2b085747033b876de3f81acc2281bbdb6640fd
Elena Neroslavskaya <lenisha@gmail.com>
2018-09-18 02:40:55 -0400

    add AlwaysEncrypted Starter

---
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/sqlserver/KeyVaultProperties.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.autoconfigure.sqlserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+
+@ConfigurationProperties("spring.datasource.always-encrypted.keyvault")
+public class KeyVaultProperties {
+
+    private String clientId;
+    private String clientSecret;
+    /**
+     * Whether allow Microsoft to collect telemetry data.
+     */
+    private boolean allowTelemetry = false;
+
+    @PostConstruct
+    public void validate() {
+        Assert.hasText(clientId, "spring.datasource.always-encrypted.keyvault.client-id must be provided");
+        Assert.hasText(clientSecret, "spring.datasource.always-encrypted.keyvault.client-secret must be provided");
+
+    }
+
+
+}
