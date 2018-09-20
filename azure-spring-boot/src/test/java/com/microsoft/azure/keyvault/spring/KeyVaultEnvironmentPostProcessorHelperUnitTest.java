25b1d861e489df3b0640d99beca845e59ed86cef
Devendra Pratap Singh <dev_ps@hotmail.com>
2018-09-20 14:06:37 +0530

    Added Unit Test for MSI Authentication  (#3)
    * Added Unit test for MSI properties

--- azure-spring-boot/src/test/java/com/microsoft/azure/keyvault/spring/KeyVaultEnvironmentPostProcessorHelperUnitTest.java
+++ azure-spring-boot/src/test/java/com/microsoft/azure/keyvault/spring/KeyVaultEnvironmentPostProcessorHelperUnitTest.java
@@ -8,31 +8,55 @@ package com.microsoft.azure.keyvault.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.EnvironmentCapable;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.core.env.EnvironmentCapable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(locations = "classpath:application.properties")
 public class KeyVaultEnvironmentPostProcessorHelperUnitTest {
 
     @Autowired
     ApplicationContext context;
 
-    @Test
-    public void testMSIAuthPropertiesNotInitialized() {
-        final MutablePropertySources sources =
-        ((ConfigurableEnvironment) context.getEnvironment()).getPropertySources();
-    
-        assertFalse("PropertySources should not contain MSI_ENDPOINT when not on Azure environment",
-        sources.contains("MSI_ENDPOINT"));
+    private KeyVaultEnvironmentPostProcessorHelper keyVaultEnvironmentPostProcessorHelper;
+    private ConfigurableEnvironment environment;
 
-        assertFalse("PropertySources should not contain MSI_SECRET when not on Azure environment",
-        sources.contains("MSI_SECRET"));
+    @Before
+    public void setup() {
+        environment = (ConfigurableEnvironment) context.getEnvironment();
+    }
+
+    @Test
+    public void testMSIAuthPropertiesNotInitialized() { 
+        assertFalse("Environment should not contain MSI_ENDPOINT when not on Azure App Service environment",
+        environment.containsProperty("MSI_ENDPOINT"));
+
+        assertFalse("Environment should not contain MSI_SECRET when not on Azure App Service environment",
+        environment.containsProperty("MSI_SECRET"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMSIAuthentication() {
+        final MutablePropertySources propertySources = environment.getPropertySources();
+        final Map msiProperties = new HashMap();
+
+        msiProperties.put("MSI_ENDPOINT", "fakeendpoint");
+        msiProperties.put("MSI_SECRET", "fakesecret");
+        propertySources.addLast(new MapPropertySource("MSI_Properties", msiProperties));
+        keyVaultEnvironmentPostProcessorHelper = 
+            new KeyVaultEnvironmentPostProcessorHelper(environment);
+        keyVaultEnvironmentPostProcessorHelper.addKeyVaultPropertySource();
     }  
 }
