6bcb2cd2e058870d99499d4a9561f0ad99ad95b8
weiping <wepa@microsoft.com>
2018-10-11 14:04:40 +0800

    upgrade storage sdk to v10

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/storage/StorageProperties.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/storage/StorageProperties.java
@@ -3,59 +3,33 @@
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
-
 package com.microsoft.azure.spring.autoconfigure.storage;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties("azure.storage")
 public class StorageProperties {
-
-    /**
-     * Azure Storage connection string.
-     */
     @NotEmpty
-    private String connectionString;
+    @Getter
+    @Setter
+    private String accountName;
 
+    @NotEmpty
+    @Getter
+    @Setter
+    private String accountKey;
+
+    @Getter
+    @Setter
+    private String containerName;
+
+    @Getter
+    @Setter
     private boolean allowTelemetry = true;
-
-    /**
-     * return allow telemery or not
-     *
-     * @return
-     */
-    public boolean isAllowTelemetry() {
-        return allowTelemetry;
-    }
-
-    /**
-     * Set allowTelemetry
-     *
-     * @param allowTelemetry
-     */
-    public void setAllowTelemetry(boolean allowTelemetry) {
-        this.allowTelemetry = allowTelemetry;
-    }
-
-    /**
-     * Get storage account connection string.
-     *
-     * @return storage account connection string
-     */
-    public String getConnectionString() {
-        return connectionString;
-    }
-
-    /**
-     * Set storage account connection string.
-     *
-     * @param connectionString
-     */
-    public void setConnectionString(String connectionString) {
-        this.connectionString = connectionString;
-    }
 }
--- azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/storage/StoragePropertiesTest.java
+++ azure-spring-boot/src/test/java/com/microsoft/azure/spring/autoconfigure/storage/StoragePropertiesTest.java
@@ -5,77 +5,55 @@
  */
 package com.microsoft.azure.spring.autoconfigure.storage;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBindException;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.bind.validation.BindValidationException;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.ObjectError;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.assertTrue;
 
 public class StoragePropertiesTest {
+    private static final String ACCOUNT_NAME_PROP = "azure.storage.account-name";
+    private static final String ACCOUNT_KEY_PROP = "azure.storage.account-key";
+    private static final String CONTAINER_NAME_PROP = "azure.storage.container-name";
 
-    private static final String CONNECTION_STRING = "some connection string";
-    private static final String CONNECTION_STRING_PROPERTY = "azure.storage.connection-string";
+    private static final String ACCOUNT_NAME = "fakeStorageAccountName";
+    private static final String ACCOUNT_KEY = "ZmFrZUFjY291bnRLZXk="; /* Base64 encoded for string fakeAccountKey */
+    private static final String CONTAINER_NAME = "fakeStorageContainerName";
 
+    private ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(StorageAutoConfiguration.class));
     @Test
     public void canSetProperties() {
-        System.setProperty(CONNECTION_STRING_PROPERTY, CONNECTION_STRING);
-
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
-            context.register(Config.class);
-            context.refresh();
-
-            final StorageProperties properties = context.getBean(StorageProperties.class);
-            assertThat(properties.getConnectionString()).isEqualTo(CONNECTION_STRING);
-        }
-
-        System.clearProperty(CONNECTION_STRING_PROPERTY);
+        contextRunner.withPropertyValues(propValuePair(ACCOUNT_NAME_PROP, ACCOUNT_NAME),
+                propValuePair(ACCOUNT_KEY_PROP, ACCOUNT_KEY), propValuePair(CONTAINER_NAME_PROP, CONTAINER_NAME))
+                .run(context -> {
+                    final StorageProperties properties = context.getBean(StorageProperties.class);
+                    assertThat(properties.getAccountName()).isEqualTo(ACCOUNT_NAME);
+                    assertThat(properties.getAccountKey()).isEqualTo(ACCOUNT_KEY);
+                    assertThat(properties.getContainerName()).isEqualTo(CONTAINER_NAME);
+                });
     }
 
     @Test
     public void emptySettingNotAllowed() {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()) {
-            Exception exception = null;
-
-            context.register(Config.class);
-
-            try {
-                context.refresh();
-            } catch (Exception e) {
-                exception = e;
-            }
-
-            assertThat(exception).isNotNull();
-            assertThat(exception).isExactlyInstanceOf(ConfigurationPropertiesBindException.class);
-
-            final BindValidationException bindException = (BindValidationException) exception.getCause().getCause();
-            final List<ObjectError> errors = bindException.getValidationErrors().getAllErrors();
-            final List<String> errorStrings = errors.stream().map(e -> e.toString()).collect(Collectors.toList());
-
-            Collections.sort(errorStrings);
-
-            final List<String> errorStringsExpected = Arrays.asList(
-                    "Field error in object 'azure.storage' on field 'connectionString': rejected value [null];"
-            );
-
-            assertThat(errorStrings.size()).isEqualTo(errorStringsExpected.size());
-
-            for (int i = 0; i < errorStrings.size(); i++) {
-                assertThat(errorStrings.get(i)).contains(errorStringsExpected.get(i));
-            }
+        try {
+            contextRunner.withPropertyValues(propValuePair(ACCOUNT_NAME_PROP, ""),
+                    propValuePair(ACCOUNT_KEY_PROP, ""))
+                    .run(context -> context.getBean(StorageProperties.class));
+        } catch (IllegalStateException e) {
+            assertThat(e.getCause().getCause()).isInstanceOf(ConfigurationPropertiesBindException.class);
         }
     }
 
-    @Configuration
-    @EnableConfigurationProperties(StorageProperties.class)
-    static class Config {
+    private static String propValuePair(String propName, String propValue) {
+        return propName + "=" + propValue;
     }
 }
