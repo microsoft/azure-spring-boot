b88d13d230ff32a50e89e2077e23e09458d306f4
weiping <wepa@microsoft.com>
2018-10-26 13:29:19 +0800

    update sample to release version 2.0.7 (#443)
    * update sample and dev version after 2.0.7 release

--- azure-spring-boot-samples/azure-cloud-foundry-service-sample/src/main/java/sample/cloudfoundry/storage/StorageRestController.java
+++ azure-spring-boot-samples/azure-cloud-foundry-service-sample/src/main/java/sample/cloudfoundry/storage/StorageRestController.java
@@ -5,11 +5,10 @@
  */
 package sample.cloudfoundry.storage;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.BlockBlobURL;
+import com.microsoft.azure.storage.blob.ContainerURL;
+import com.microsoft.azure.storage.blob.TransferManager;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +20,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.channels.AsynchronousFileChannel;
 
+@SuppressFBWarnings({"RV_RETURN_VALUE_IGNORED"})
 @RestController
 public class StorageRestController {
 
@@ -33,8 +34,9 @@ public class StorageRestController {
             "https://raw.githubusercontent.com/mjeffries-pivotal/pcf-samples/master/images/azure-pcf.jpg";
     private static final Logger LOG = LoggerFactory
             .getLogger(StorageRestController.class);
+
     @Autowired
-    private CloudStorageAccount account;
+    private ContainerURL containerURL;
 
     @RequestMapping(value = "/blob", method = RequestMethod.GET)
     @ResponseBody
@@ -43,36 +45,34 @@ public class StorageRestController {
 
         try {
             LOG.info("showBlob start");
-            if (account == null) {
-                LOG.error("Storage Account is null!");
+            if (containerURL == null) {
+                LOG.error("ContainerURL is null!");
                 return;
             }
 
             final URL u = new URL(IMAGE_PATH);
             is = u.openStream();
             response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-            final int imageSize = IOUtils.copy(is, response.getOutputStream());
+            IOUtils.copy(is, response.getOutputStream());
 
-            LOG.debug("Connecting to storage account...");
-            final CloudBlobClient serviceClient = account
-                    .createCloudBlobClient();
-
-            // Container name must be lower case.
-            final CloudBlobContainer container = serviceClient
-                    .getContainerReference("myimages");
-            container.createIfNotExists();
+            // Create container.
+            containerURL.create(null, null, null);
 
             // Upload an image file.
             LOG.debug("Uploading image...");
-            final CloudBlockBlob blob = container
-                    .getBlockBlobReference("image1.jpg");
-            blob.upload(new URL(IMAGE_PATH).openStream(), imageSize);
-            LOG.debug("Uploading image complete");
+            final BlockBlobURL blockBlobURL = containerURL.createBlockBlobURL("image1.jpg");
+            final File imageFile = new File(IMAGE_PATH);
+            final AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(imageFile.toPath());
+
+            TransferManager.uploadFileToBlockBlob(fileChannel, blockBlobURL, 8 * 1024 * 1024, null)
+                    .subscribe(r -> {
+                        LOG.debug("Uploading image complete");
+                    }, error -> {
+                        LOG.error("Failed to upload image", error);
+                    });
 
         } catch (IOException e) {
             LOG.error("Error retrieving image", e);
-        } catch (URISyntaxException | StorageException e) {
-            LOG.error("Error accessing azure storage container", e);
         } finally {
             if (is != null) {
                 try {
