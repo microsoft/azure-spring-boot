03825fe89580ffd4777ccfce260bb6e685f73ab6
Darren Forsythe <git@darrenforsythe.com>
2018-12-18 01:11:12 +0000

    Cannot override ServiceEndpoint Properties (#503)
    Remove Yml YamlFileApplicationContextInitializer. Remap yml to properties file. Add PropertySource to ADD Autoconfiguration. Use BeanDefinition constants for scope. Add tests to verify expected behavior

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAuthenticationFilterAutoConfiguration.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAuthenticationFilterAutoConfiguration.java
@@ -11,12 +11,14 @@ import com.nimbusds.jose.util.DefaultResourceRetriever;
 import com.nimbusds.jose.util.ResourceRetriever;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.util.ClassUtils;
 
@@ -26,6 +28,7 @@ import java.util.HashMap;
 @ConditionalOnWebApplication
 @ConditionalOnProperty(prefix = "azure.activedirectory", value = {"client-id", "client-secret"})
 @EnableConfigurationProperties({AADAuthenticationProperties.class, ServiceEndpointsProperties.class})
+@PropertySource(value = "classpath:serviceEndpoints.properties")
 public class AADAuthenticationFilterAutoConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(AADAuthenticationProperties.class);
 
@@ -47,7 +50,7 @@ public class AADAuthenticationFilterAutoConfiguration {
      * @return AADAuthenticationFilter bean
      */
     @Bean
-    @Scope("singleton")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
     @ConditionalOnMissingBean(AADAuthenticationFilter.class)
     public AADAuthenticationFilter azureADJwtTokenFilter() {
         LOG.info("AzureADJwtTokenFilter Constructor.");
@@ -56,7 +59,7 @@ public class AADAuthenticationFilterAutoConfiguration {
     }
 
     @Bean
-    @Scope("singleton")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
     @ConditionalOnMissingBean(ResourceRetriever.class)
     public ResourceRetriever getJWTResourceRetriever() {
         return new DefaultResourceRetriever(aadAuthProps.getJwtConnectTimeout(), aadAuthProps.getJwtReadTimeout(),
