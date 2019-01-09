16bb74102fe1d2de699ebf242d9dab81a2ecf5a5
Wladislaw Mitzel <wmitzel@airplus.com>
2019-01-09 18:32:51 +0100

    Remove unused import.

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAppRoleAuthenticationFilterAutoConfiguration.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/AADAppRoleAuthenticationFilterAutoConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ConditionalOnWebApplication
