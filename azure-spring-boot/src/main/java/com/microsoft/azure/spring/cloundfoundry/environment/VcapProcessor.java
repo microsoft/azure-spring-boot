1e47babccf1aa19c729dbaa06db300d50def88ac
Pan Li <panli@microsoft.com>
2019-01-14 09:56:08 +0800

    Remove the dependency of jackson in VcapProcessor.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloundfoundry/environment/VcapProcessor.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloundfoundry/environment/VcapProcessor.java
@@ -5,18 +5,20 @@
  */
 package com.microsoft.azure.spring.cloundfoundry.environment;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -41,68 +43,98 @@ public class VcapProcessor implements EnvironmentPostProcessor {
     private boolean logFlag = false;
 
     @Override
-    public void postProcessEnvironment(ConfigurableEnvironment confEnv,
-                                       SpringApplication app) {
+    public void postProcessEnvironment(ConfigurableEnvironment confEnv, SpringApplication app) {
         final Map<String, Object> environment = confEnv.getSystemEnvironment();
-
         final String logValue = (String) environment.get(VcapProcessor.LOG_VARIABLE);
+
         if ("true".equals(logValue)) {
             logFlag = true;
         }
 
         log("VcapParser.postProcessEnvironment: Start");
-        final String vcapServices = (String) environment
-                .get(VcapProcessor.VCAP_SERVICES);
+
+        final String vcapServices = (String) environment.get(VcapProcessor.VCAP_SERVICES);
         final VcapResult result = parse(vcapServices);
+
         result.setLogFlag(logFlag);
         result.setConfEnv(confEnv);
         result.populateProperties();
+
         log("VcapParser.postProcessEnvironment: End");
+    }
+
+    @SuppressWarnings("unchecked")
+    private VcapServiceConfig getVcapServiceConfig(@NonNull Map<String, Object> configMap) {
+        final VcapServiceConfig serviceConfig = new VcapServiceConfig();
+
+        serviceConfig.setLabel((String) configMap.getOrDefault("label", null));
+        serviceConfig.setName((String) configMap.getOrDefault("name", null));
+        serviceConfig.setProvider((String) configMap.getOrDefault("provider", null));
+        serviceConfig.setSyslogDrainUrl((String) configMap.getOrDefault("syslog_drain_url", null));
+        serviceConfig.setPlan((String) configMap.getOrDefault("plan", null));
+
+        final List<String> tags = (List<String>) configMap.get("tags");
+        final List<String> volumeMounts = (List<String>) configMap.get("volume_mounts");
+
+        if (tags != null) {
+            serviceConfig.setTags(tags.toArray(new String[0]));
+        }
+
+        if (volumeMounts != null) {
+            serviceConfig.setVolumeMounts(volumeMounts.toArray(new String[0]));
+        }
+
+        serviceConfig.setCredentials((Map<String, String>) configMap.get("credentials"));
+
+        return serviceConfig;
+    }
+
+    private List<VcapServiceConfig> getVcapServiceConfigList(@NonNull Object value) {
+        Assert.isInstanceOf(List.class, value);
+        @SuppressWarnings("unchecked") final List<Map<String, Object>> configs = (List<Map<String, Object>>) value;
+
+        return configs.stream().map(this::getVcapServiceConfig).collect(Collectors.toList());
     }
 
     public VcapResult parse(String vcapServices) {
         final VcapResult result = new VcapResult();
-
         final List<VcapPojo> results = new ArrayList<>();
 
         log("VcapParser.parse:  vcapServices = " + vcapServices);
-        if (vcapServices != null) {
+
+        if (StringUtils.hasText(vcapServices)) {
             try {
-                final ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false);
+                final JsonParser parser = JsonParserFactory.getJsonParser();
+                final Map<String, Object> servicesMap = parser.parseMap(vcapServices);
+                final Set<Map.Entry<String, Object>> services = servicesMap.entrySet();
 
-                final Map<String, List<VcapServiceConfig>> servicesMap = objectMapper.readValue(vcapServices,
-                        new TypeReference<Map<String, List<VcapServiceConfig>>>() { });
-                final Set<Map.Entry<String, List<VcapServiceConfig>>> services = servicesMap.entrySet();
+                Assert.notNull(services, "Services entrySet cannot be null.");
 
-                if (services != null) {
-                    final Iterator<Map.Entry<String, List<VcapServiceConfig>>> serviceIterator = services.iterator();
+                for (final Map.Entry<String, Object> serviceEntry : services) {
+                    final String name = serviceEntry.getKey();
 
-                    while (serviceIterator.hasNext()) {
-                        final Map.Entry<String, List<VcapServiceConfig>> serviceEntry = serviceIterator.next();
-                        final String name = serviceEntry.getKey();
+                    if (name.startsWith(AZURE) || USER_PROVIDED.equals(name)) {
+                        Assert.isInstanceOf(List.class, serviceEntry.getValue());
+                        final List<VcapServiceConfig> azureServices = getVcapServiceConfigList(serviceEntry.getValue());
 
-                        if (name.startsWith(AZURE) || USER_PROVIDED.equals(name)) {
-                            final List<VcapServiceConfig> azureServices = serviceEntry.getValue();
-
-                            results.addAll(azureServices.stream()
-                                    .map(service -> parseService(name, service, vcapServices))
-                                    .filter(vcapPojo -> vcapPojo != null).collect(Collectors.toList()));
-                        }
+                        results.addAll(
+                                azureServices.stream()
+                                        .map(service -> parseService(name, service, vcapServices))
+                                        .filter(Objects::nonNull).collect(Collectors.toList())
+                        );
                     }
                 }
-            } catch (IOException e) {
+            } catch (JsonParseException e) {
                 LOGGER.error("Error parsing " + vcapServices, e);
             }
         }
 
-        result.setPojos(results.toArray(new VcapPojo[results.size()]));
+        result.setPojos(results.toArray(new VcapPojo[0]));
         return result;
     }
 
     private VcapPojo parseService(String serviceBrokerName, VcapServiceConfig serviceConfig, String vCapServices) {
         final VcapPojo result = new VcapPojo();
-
         final Map<String, String> credentials = serviceConfig.getCredentials();
 
         if (USER_PROVIDED.equals(serviceBrokerName)) {
