5d2a2478793e97e63b3916c6f3b310ba93560b27
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2019-02-11 21:34:09 -0600

    Add handler for sign page forgot password defect (#561)
    * Take care of service defect of forgot password scenario.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/b2c/AADB2CAutoConfiguration.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/b2c/AADB2CAutoConfiguration.java
@@ -23,7 +23,8 @@ import org.springframework.util.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.microsoft.azure.spring.autoconfigure.b2c.AADB2CProperties.*;
+import static com.microsoft.azure.spring.autoconfigure.b2c.AADB2CProperties.POLICY_SIGN_UP_OR_SIGN_IN;
+import static com.microsoft.azure.spring.autoconfigure.b2c.AADB2CProperties.PREFIX;
 
 @Configuration
 @ConditionalOnWebApplication
@@ -53,7 +54,7 @@ public class AADB2CAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public AADB2CAuthorizationRequestResolver b2cOAuth2AuthorizationRequestResolver() {
-        return new AADB2CAuthorizationRequestResolver(repository);
+        return new AADB2CAuthorizationRequestResolver(repository, properties.getPolicies().getPasswordReset());
     }
 
     @Bean
