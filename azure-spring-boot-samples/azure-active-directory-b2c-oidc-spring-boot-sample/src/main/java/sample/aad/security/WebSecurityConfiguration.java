0df9693091d38c3a6121a14be948441b7461ae7d
Pan Li <panli@microsoft.com>
2019-02-16 23:06:43 +0800

    Add b2c Oidc login Configurer.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- azure-spring-boot-samples/azure-active-directory-b2c-oidc-spring-boot-sample/src/main/java/sample/aad/security/WebSecurityConfiguration.java
+++ azure-spring-boot-samples/azure-active-directory-b2c-oidc-spring-boot-sample/src/main/java/sample/aad/security/WebSecurityConfiguration.java
@@ -5,9 +5,7 @@
  */
 package sample.aad.security;
 
-import com.microsoft.azure.spring.autoconfigure.b2c.AADB2CAuthorizationRequestResolver;
-import com.microsoft.azure.spring.autoconfigure.b2c.AADB2CLogoutSuccessHandler;
-import com.microsoft.azure.spring.autoconfigure.b2c.AADB2CProperties;
+import com.microsoft.azure.spring.autoconfigure.b2c.AADB2COidcLoginConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,33 +13,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private AADB2CProperties properties;
+    private final AADB2COidcLoginConfigurer configurer;
 
-    private AADB2CLogoutSuccessHandler logoutSuccessHandler;
-
-    private AADB2CAuthorizationRequestResolver requestResolver;
-
-    public WebSecurityConfiguration(AADB2CProperties properties,
-                                    AADB2CLogoutSuccessHandler logoutSuccessHandler,
-                                    AADB2CAuthorizationRequestResolver requestResolver) {
-        this.properties = properties;
-        this.logoutSuccessHandler = logoutSuccessHandler;
-        this.requestResolver = requestResolver;
+    public WebSecurityConfiguration(AADB2COidcLoginConfigurer configurer) {
+        this.configurer = configurer;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                // Expire the token from b2c side.
-                .logout().logoutSuccessHandler(logoutSuccessHandler)
-                .and()
-                .oauth2Login()
-                // Customize oauth2 processing url from b2c configuration.
-                .loginProcessingUrl(properties.getLoginProcessingUrl())
-                // Customize oauth2 authorization request.
-                .authorizationEndpoint().authorizationRequestResolver(requestResolver);
+                .apply(configurer)
+        ;
     }
 }
