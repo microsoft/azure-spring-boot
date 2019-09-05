7106feea93ccb06fb4f9610ab95e2fe9fa1d776a
Julien Dubois <julien.dubois@gmail.com>
2019-09-05 22:10:18 +0200

    Refactor package name "cloundfoundry" to "cloudfoundry"
    Fix #713

--- azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloudfoundry/environment/VcapServiceType.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/spring/cloudfoundry/environment/VcapServiceType.java
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
-package com.microsoft.azure.spring.cloundfoundry.environment;
+package com.microsoft.azure.spring.cloudfoundry.environment;
 
 public enum VcapServiceType {
     AZURE_REDISCACHE("azure-rediscache"), AZURE_DOCUMENTDB("azure-documentdb"), AZURE_STORAGE(
