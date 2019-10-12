4f95b08e81fdc31e9b873339d0e2ada6b240aefa
Zhou Liu <sumeng.liuz@gmail.com>
2019-10-12 17:14:07 +0800

    1.add specific secret keys mode 2.add token cache 3.change hashmap 2 list in propertySource

---
+++ azure-spring-boot/src/main/java/com/microsoft/azure/keyvault/spring/AADAuthUtil.java
@@ -0,0 +1,28 @@
+package com.microsoft.azure.keyvault.spring;
+
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.*;
+
+public class AADAuthUtil {
+    public AuthenticationResult getToken(String authorization, String resource, String clientId, String clientKey, long tokenAcquireTimeout) {
+        AuthenticationContext context = null;
+        AuthenticationResult result = null;
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            context = new AuthenticationContext(authorization, false, executorService);
+            final ClientCredential credential = new ClientCredential(clientId, clientKey);
+
+            final Future<AuthenticationResult> future = context.acquireToken(resource, credential, null);
+            result = future.get(tokenAcquireTimeout, TimeUnit.SECONDS);
+        } catch (MalformedURLException | TimeoutException | InterruptedException | ExecutionException ex) {
+            throw new IllegalStateException("Failed to do authentication.", ex);
+        } finally {
+            executorService.shutdown();
+        }
+        return result;
+    }
+}
--- azure-spring-boot/src/main/java/com/microsoft/azure/keyvault/spring/Constants.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/keyvault/spring/Constants.java
@@ -17,6 +17,7 @@ public class Constants {
     public static final String AZURE_KEYVAULT_ENABLED = "azure.keyvault.enabled";
     public static final String AZURE_KEYVAULT_VAULT_URI = "azure.keyvault.uri";
     public static final String AZURE_KEYVAULT_REFRESH_INTERVAL = "azure.keyvault.refresh-interval";
+    public static final String AZURE_KEYVAULT_SECRET_KEYS = "azure.keyvault.secret.keys";
     public static final String AZURE_KEYVAULT_PROPERTYSOURCE_NAME = "azurekv";
     public static final String AZURE_TOKEN_ACQUIRE_TIMEOUT_IN_SECONDS = "azure.keyvault.token-acquire-timeout-seconds";
     public static final String AZURE_KEYVAULT_ALLOW_TELEMETRY = "azure.keyvault.allow.telemetry";
