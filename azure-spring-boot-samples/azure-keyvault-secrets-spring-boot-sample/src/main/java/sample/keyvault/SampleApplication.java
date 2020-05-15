af2d3f4b61a6492a35711ce7a8f9db07f1f389aa
Rujun Chen <rujche@microsoft.com>
2020-05-15 10:27:38 +0800

    Make the sample simple: Just keep the basic function. (#637)

--- azure-spring-boot-samples/azure-keyvault-secrets-spring-boot-sample/src/main/java/sample/keyvault/SampleApplication.java
+++ azure-spring-boot-samples/azure-keyvault-secrets-spring-boot-sample/src/main/java/sample/keyvault/SampleApplication.java
@@ -6,41 +6,23 @@
 
 package sample.keyvault;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class SampleApplication implements CommandLineRunner {
 
-    @Value("${yourSecretPropertyName9}")
-    private String yourSecretPropertyName9;
-
-    @Value("${property.in.raw}")
-    private String propertyInRaw;
-
-    @Value("${property.refer.to.another.property}")
-    private String propertyReferToAnotherProperty;
-
-    @Value("${property.refer.to.another.property.another}")
-    private String propertyReferToAnotherPropertyAnother;
-
-    @Value("${key-vault-value-contain-spel}")
-    private String keyVaultValueContainSpel;
+    @Value("${yourSecretPropertyName}")
+    private String yourSecretPropertyName;
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
 
-    public void run(String[] args) {
-        log.info("yourSecretPropertyName9 = {}", yourSecretPropertyName9);
-        log.info("propertyInRaw = {}", propertyInRaw);
-        log.info("propertyReferToAnotherProperty = {}", propertyReferToAnotherProperty);
-        log.info("propertyReferToAnotherPropertyAnother = {}", propertyReferToAnotherPropertyAnother);
-        log.info("keyVaultKeyContainSpel = {}", keyVaultValueContainSpel);
+    public void run(String[] args) throws Exception {
+        System.out.println("property yourSecretPropertyName value is: " + yourSecretPropertyName);
     }
 
 }
