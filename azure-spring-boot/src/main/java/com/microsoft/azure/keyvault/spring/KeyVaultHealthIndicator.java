bb5320f0992f64d4987bb95518a1e40cc74c710c
Manfred Riem <manfred.riem@microsoft.com>
2020-05-21 12:38:56 -0600

    Reworked using @AutoWired

--- azure-spring-boot/src/main/java/com/microsoft/azure/keyvault/spring/KeyVaultHealthIndicator.java
+++ azure-spring-boot/src/main/java/com/microsoft/azure/keyvault/spring/KeyVaultHealthIndicator.java
@@ -5,24 +5,25 @@
  */
 package com.microsoft.azure.keyvault.spring;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Iterator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
+/**
+ * The custom Azure Key Vault /health indicator.
+ */
 @Component
-public class KeyVaultHealthIndicator
-        implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, HealthIndicator {
+public class KeyVaultHealthIndicator implements HealthIndicator {
 
     /**
-     * Stores the configurable environment.
+     * Stores the environment we need.
      */
-    private static ConfigurableEnvironment environment;
+    @Autowired
+    private ConfigurableEnvironment environment;
 
     /**
      * Constructor.
@@ -54,16 +55,5 @@ public class KeyVaultHealthIndicator
             result = Health.up().build();
         }
         return result;
-    }
-
-    /**
-     * Handle the application environment prepared event.
-     *
-     * @param event the event.
-     */
-    @Override
-    @SuppressFBWarnings
-    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
-        environment = event.getEnvironment();
     }
 }
